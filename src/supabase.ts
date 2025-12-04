33ae28e0148907eb0e5279043a54931699dc4b8f
Paulo Chiosa <chiosa@MacBook-Air-de-Paulo.local>
2025-12-03 19:28:20 -0500

    Hardcode Supabase keys

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,6 +1,8 @@
+// src/supabase.ts
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+VITE_SUPABASE_URL = "https://wfdafaaajtemwtupsvws.supabase.co";
+VITE_SUPABASE_ANON_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndmZGFmYWFhanRlbXd0dXBzdndzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQzMDE4MjAsImV4cCI6MjA3OTg3NzgyMH0.I2X2COdY8HH5iWE9vU_Jjd59FIKuTWVDfFXZbVfhP3s";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
