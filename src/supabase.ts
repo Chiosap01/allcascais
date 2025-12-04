9d61404055a4cf2ff926c36b30ecf6430eaab365
Paulo Chiosa <chiosa@MacBook-Air-de-Paulo.local>
2025-12-03 19:58:29 -0500

    Fix Supabase env vars

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,8 +1,11 @@
 // src/supabase.ts
 import { createClient } from "@supabase/supabase-js";
 
-VITE_SUPABASE_URL = "https://wfdafaaajtemwtupsvws.supabase.co";
-VITE_SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndmZGFmYWFhanRlbXd0dXBzdndzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQzMDE4MjAsImV4cCI6MjA3OTg3NzgyMH0.I2X2COdY8HH5iWE9vU_Jjd59FIKuTWVDfFXZbVfhP3s";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Missing Supabase environment variables");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
